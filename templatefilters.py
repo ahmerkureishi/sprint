d88f4b826714daebeb9e2135f41c08e91f0cbfeb
Todd Webb <twebb.goblue@gmail.com>
2008-06-04 11:23:52 -0500

    Refactoring: removed tab indentation and escaped all text fields against cross-site scripting attacks

--- templatefilters.py
+++ templatefilters.py
@@ -1,13 +1,13 @@
 from google.appengine.ext.webapp import template
 
 def truncate(value, size):
-	return value[0:size]
+  return value[0:size]
 
 def truncate_dot(value, size):
-	if len(value) > size and size > 3:
-		return value[0:(size-3)] + '...'
-	else:
-		return value[0:size]
+  if len(value) > size and size > 3:
+    return value[0:(size-3)] + '...'
+  else:
+    return value[0:size]
 
 register = template.create_template_register()
 register.filter(truncate)
